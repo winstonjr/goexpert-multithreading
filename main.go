cff3c24e83f25de274e324ad3448ebf4f4388adb
Winston Pacheco Junior <winston@atleastonce.cc>
2024-07-05 18:15:19 -0300

    utilizando apenas o método que eu entendo como mais legal

--- main.go
+++ main.go
@@ -16,42 +16,6 @@ func main() {
 	}
 	cep := os.Args[1]
 
-	// Usando o método 1
-	resultChViaCEP := make(chan string)
-	defer close(resultChViaCEP)
-	resultChBrasilAPI := make(chan string)
-	defer close(resultChBrasilAPI)
-
-	go func() {
-		result, err := MakeRequest("viacep", "https://viacep.com.br/ws/"+cep+"/json/")
-		if err != nil {
-			log.Println("Erro ao buscar na viacep:", err)
-			return
-		}
-		resultChViaCEP <- result
-	}()
-
-	go func() {
-		result, err := MakeRequest("brasilapi", "https://brasilapi.com.br/api/cep/v1/"+cep)
-		if err != nil {
-			log.Println("Erro ao buscar na brasilapi:", err)
-			return
-		}
-		resultChBrasilAPI <- result
-	}()
-
-	fmt.Println("==> Resultado do método 1")
-	select {
-	case resultViaCEP := <-resultChViaCEP:
-		fmt.Println(resultViaCEP)
-	case resultBrasilAPI := <-resultChBrasilAPI:
-		fmt.Println(resultBrasilAPI)
-	case <-time.After(1 * time.Second):
-		fmt.Println("Erro de timeout")
-	}
-	fmt.Println("##########################################")
-
-	// usando o método 2
 	resultChViaCEP2 := make(chan string)
 	defer close(resultChViaCEP2)
 	resultChBrasilAPI2 := make(chan string)
@@ -60,7 +24,6 @@ func main() {
 	go MakeRequest2("viacep", "https://viacep.com.br/ws/"+cep+"/json/", resultChViaCEP2)
 	go MakeRequest2("brasilapi", "https://brasilapi.com.br/api/cep/v1/"+cep, resultChBrasilAPI2)
 
-	fmt.Println("==> Resultado do método 2")
 	select {
 	case resultViaCEP := <-resultChViaCEP2:
 		fmt.Println(resultViaCEP)
@@ -69,22 +32,6 @@ func main() {
 	case <-time.After(1 * time.Second):
 		fmt.Println("Erro de timeout")
 	}
-	fmt.Println("##########################################")
-}
-
-func MakeRequest(apiName string, url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("Resultado da %s: %s", apiName, string(body)), nil
 }
 
 func MakeRequest2(apiName string, url string, resultChannel chan<- string) {
